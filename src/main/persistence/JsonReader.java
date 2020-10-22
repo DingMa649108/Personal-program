5241e303234c3d50b98ed165ab2987605f538b55
Ding Ma <mading11@students.cs.ubc.ca>
2020-10-21 17:05:16 -0700

    Ding's Personal Project

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -1,5 +1,6 @@
 package persistence;
 
+import model.Job;
 import model.Role;
 
 import java.io.IOException;
@@ -13,7 +14,27 @@ import org.json.*;
 
 // Represents a reader that reads workroom from JSON data stored in file
 public class JsonReader {
-    private String source;
+    private final String source;
+    private String name;
+    private int age;
+    private String gender;
+    private String job;
+    protected int strength;
+    protected int constitution;
+    protected int power;
+    protected int dexterity;
+    protected int appearance;
+    protected int size;
+    protected int intelligence;
+    protected int education;
+    protected int luck;
+    protected int freeSkillPoints;
+    private int bonusDamage;
+    private int hp;
+    private int movement;
+    private int sanity;
+    private int credit;
+    Role role;
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
@@ -42,26 +63,67 @@ public class JsonReader {
 
     // EFFECTS: parses workroom from JSON object and returns it
     private Role parseWorkRoom(JSONObject jsonObject) {
-        String name = jsonObject.getString("name");
-        int age = jsonObject.getInt("age");
-        String gender = jsonObject.getString("gender");
-        String job = jsonObject.getString("job");
-        int strength = jsonObject.getInt("STR");
-        int constitution = jsonObject.getInt("CON");
-        int power = jsonObject.getInt("POW");
-        int dexterity = jsonObject.getInt("DEX");
-        int appearance = jsonObject.getInt("APP");
-        int size = jsonObject.getInt("SIZ");
-        int intelligence = jsonObject.getInt("INT");
-        int education = jsonObject.getInt("EDU");
-        int luck = jsonObject.getInt("LUC");
-        int freeSkillPoints = jsonObject.getInt("free skill points");
-        int bonusDamage = jsonObject.getInt("bonus damage");
-        int hp = jsonObject.getInt("hp");
-        int movement = jsonObject.getInt("MOV");
-        int sanity = jsonObject.getInt("SAN");
-        int credit = jsonObject.getInt("credit");
+        name = jsonObject.getString("name");
+        age = jsonObject.getInt("age");
+        gender = jsonObject.getString("gender");
+        job = jsonObject.getString("job");
+        strength = jsonObject.getInt("STR");
+        constitution = jsonObject.getInt("CON");
+        power = jsonObject.getInt("POW");
+        dexterity = jsonObject.getInt("DEX");
+        appearance = jsonObject.getInt("APP");
+        size = jsonObject.getInt("SIZ");
+        intelligence = jsonObject.getInt("INT");
+        education = jsonObject.getInt("EDU");
+        luck = jsonObject.getInt("LUC");
+        freeSkillPoints = jsonObject.getInt("free skill points");
+        bonusDamage = jsonObject.getInt("bonus damage");
+        hp = jsonObject.getInt("hp");
+        movement = jsonObject.getInt("MOV");
+        sanity = jsonObject.getInt("SAN");
+        credit = jsonObject.getInt("credit");
         Role role = new Role();
+        setRole(role, jsonObject);
+        return role;
+    }
+
+
+    // MODIFIES: wr
+    // EFFECTS: parses thingies from JSON object and adds them to role
+    private void addSkills(Role role, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("skills");
+        for (Object json : jsonArray) {
+            JSONObject nextSkill = (JSONObject) json;
+            addSkill(role, nextSkill);
+        }
+    }
+
+    // MODIFIES: wr
+    // EFFECTS: parses thingy from JSON object and adds it to role
+    private void addSkill(Role role, JSONObject jsonObject) {
+        String skillName = jsonObject.getString("Skill name");
+        int skillPoints = jsonObject.getInt("Skill points");
+        role.addSkills(new Skill(skillName, skillPoints));
+    }
+
+    // MODIFIES: wr
+    // EFFECTS: parses thingies from JSON object and adds them to role
+    private void addItems(Role role, JSONObject jsonObject) {
+        JSONArray jsonArray = jsonObject.getJSONArray("items");
+        for (Object json : jsonArray) {
+            JSONObject nextItem = (JSONObject) json;
+            addItem(role, nextItem);
+        }
+    }
+
+    // MODIFIES: wr
+    // EFFECTS: parses thingy from JSON object and adds it to role
+    private void addItem(Role role, JSONObject jsonObject) {
+        String itemName = jsonObject.getString("Item name");
+        role.addItem(itemName);
+    }
+
+    private void setRole(Role role, JSONObject jsonObject) {
         role.setStrength(strength);
         role.setDexterity(dexterity);
         role.setAge(age);
@@ -83,43 +145,6 @@ public class JsonReader {
         role.setJobName(job);
         addSkills(role, jsonObject);
         addItems(role, jsonObject);
-        return role;
-    }
-
-
-    // MODIFIES: wr
-    // EFFECTS: parses thingies from JSON object and adds them to role
-    private void addSkills(Role role, JSONObject jsonObject) {
-        JSONArray jsonArray = jsonObject.getJSONArray("skills");
-        for (Object json : jsonArray) {
-            JSONObject nextSkill = (JSONObject) json;
-            addSkill(role, nextSkill);
-        }
-    }
-
-    // MODIFIES: wr
-    // EFFECTS: parses thingy from JSON object and adds it to role
-    private void addSkill(Role role, JSONObject jsonObject) {
-        String skillName = jsonObject.getString("Skill name");
-        int skillPoints = jsonObject.getInt("Skill points");
-        role.addSkills(new Skill(skillName,skillPoints));
-    }
-
-    // MODIFIES: wr
-    // EFFECTS: parses thingies from JSON object and adds them to role
-    private void addItems(Role role, JSONObject jsonObject) {
-        JSONArray jsonArray = jsonObject.getJSONArray("items");
-        for (Object json : jsonArray) {
-            JSONObject nextItem = (JSONObject) json;
-            addItem(role, nextItem);
-        }
-    }
-
-    // MODIFIES: wr
-    // EFFECTS: parses thingy from JSON object and adds it to role
-    private void addItem(Role role, JSONObject jsonObject) {
-        String itemName = jsonObject.getString("Item name");
-        role.addItem(itemName);
     }
 }
 
